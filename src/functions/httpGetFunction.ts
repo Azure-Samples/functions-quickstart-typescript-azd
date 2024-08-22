4f114f462a3d248bd3cd62ac13a20b3ddb95c54d
Glenn Gailey <ggailey777@hotmail.com>
2024-08-22 10:59:45 -0700

    Initial commit w/o history

---
+++ src/functions/httpGetFunction.ts
@@ -0,0 +1,15 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function httpGetFunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('httpGetFunction', {
+    methods: ['GET'],
+    authLevel: 'function',
+    handler: httpGetFunction
+});
